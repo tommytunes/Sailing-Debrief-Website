7b699d88c72d46de4f5b15122c4a0c823175112b
Thomas Staples <thomasstaples99@gmail.com>
2026-05-05 12:14:09 -0400

    new download links

--- src/pages/Download/Download.jsx
+++ src/pages/Download/Download.jsx
@@ -6,9 +6,9 @@ const Download = () => {
             <h1 className="font-mono mb-5 text-blue-500">Download</h1>
             <h1 className="mb-30 text-6xl font-bold">Sailing Debrief {APP_VERSION}</h1>
             <div className="flex flex-row gap-4">
-                <DownloadCard title={'MacOs Apple Silicon'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.1.1/Sailing.Debrief-1.1.1-arm64.dmg'} />
-                <DownloadCard title={'MacOs Intel'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.1.1/Sailing.Debrief-1.1.1.dmg'} />
-                <DownloadCard title={'Windows 10 & 11'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.1.1/Sailing.Debrief.Setup.1.1.1.exe'} />
+                <DownloadCard title={'MacOs Apple Silicon'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.2.1/Sailing.Debrief-1.2.1-arm64.dmg'} />
+                <DownloadCard title={'MacOs Intel'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.2.1/Sailing.Debrief-1.2.1.dmg'} />
+                <DownloadCard title={'Windows 10 & 11'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.2.1/Sailing.Debrief.Setup.1.2.1.exe'} />
             </div>
         </div>
         
