50912d1e41bb1ee6cc43464f619745801f7c3441
Thomas Staples <thomasstaples99@gmail.com>
2026-05-20 10:53:08 +0200

    card w

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -110,7 +110,7 @@ const Dashboard = () => {
 
      return (
          <div className="p-10 flex flex-col items-center">
-            <div className="card bg-white card-border w-[80vw]">
+            <div className="card bg-white card-border w-100 lg:w-[80vw]">
                 <div className="flex items-center justify-between px-6 py-6">
                     <div className="flex flex-col">
                         <h1 className="text-2xl lg:text-4xl font-bold">Welcome, {name}</h1>
