900596917e173fb238682355e8750f3581a10eff
Thomas Staples <thomasstaples99@gmail.com>
2026-05-20 10:47:02 +0200

    download font size

--- src/pages/Download/Download.jsx
+++ src/pages/Download/Download.jsx
@@ -7,7 +7,7 @@ const Download = () => {
     return (
         <div className="flex flex-col items-center">
             <h1 className="font-mono mb-5 text-blue-500">Download</h1>
-            <h1 className="mb-30 text-5xl lg:text-6xl font-bold">Sailing Debrief {APP_VERSION}</h1>
+            <h1 className="mb-30 text-4xl lg:text-6xl font-bold">Sailing Debrief {APP_VERSION}</h1>
             <div className="flex flex-col lg:flex-row gap-4">
                 <DownloadCard title={'MacOs Apple Silicon'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.2.2/Sailing.Debrief-1.2.2-arm64.dmg'} />
                 <DownloadCard title={'MacOs Intel'} filePath={'https://github.com/tommytunes/Sailing-Debrief-Website/releases/download/1.2.2/Sailing.Debrief-1.2.2.dmg'} />
